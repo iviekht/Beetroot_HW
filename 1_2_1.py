f95a0abf22d258e3799204d042ed2300d26be56b
Iryna Viekhtieva <91473252+iviekht@users.noreply.github.com>
2021-10-02 17:29:03 +0300

    Update 1_2_1.py

--- 1_2_1.py
+++ 1_2_1.py
@@ -1,4 +1,12 @@
 name='Iryna'
 day='Tuesday'
+#1
 print('Good day, '+ name +'! '+ day + ' is a perfect day to learn some python')
-print('Good day, %s! %s is a perfect day to learn some python' %(name,day))
+#2
+print('Good day, %s! %s is a perfect day to learn some python' %(name, day))
+#3
+print (f'Good day, {name}! {day} is a perfect day to learn some python')
+#4
+print('Good day, {}! {} is a perfect day to learn some python'.format(name, day))
+#5
+print('Good day, {1}. {0} is a perfect day to learn some python'.format(day, name))
