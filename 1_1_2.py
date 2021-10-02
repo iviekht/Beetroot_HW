ed4f2b5c7fac9c363b1a5eccac77f336a50ddd87
Iryna Viekhtieva <91473252+iviekht@users.noreply.github.com>
2021-10-02 13:12:25 +0300

    Update 1_1_2.py

--- 1_1_2.py
+++ 1_1_2.py
@@ -1,4 +1,4 @@
 #Good Morning, dear
 print('Good Morning, dear', end='\n \n')
 # Hi, Welcome to Chilli`s!
-print ('Hi,', 'Welcome', 'to', 'Chilli`s', sep=' ', end='!')
+print ('Hi,', 'Welcome', 'to', "Chilli's", sep=' ', end='!')
